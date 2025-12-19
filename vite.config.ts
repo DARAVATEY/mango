99680bbf5467861a58472f0bf7e0809eb723ce3f
DARAVATEY <n.daravatey02@gmail.com>
2025-12-20 00:59:56 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-# Step 1: Build the app
-FROM node:20-slim AS build
-WORKDIR /app
-COPY package*.json ./
-RUN npm install
-COPY . .
-RUN npm run build
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-# Step 2: Serve with Nginx
-FROM nginx:alpine
-COPY --from=build /app/dist /usr/share/nginx/html
-# Cloud Run expects traffic on port 8080 by default
-EXPOSE 8080
-# Update nginx to listen on 8080
-RUN sed -i 's/listen[[:space:]]*80;/listen 8080;/g' /etc/nginx/conf.d/default.conf
-CMD ["nginx", "-g", "daemon off;"]
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 8080,
+    host: true
+  }
+})
